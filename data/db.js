798b06eca21fda7d886597cc641c438d9da8d172
Elijah Atkins <elijahorama@gmail.com>
2020-07-08 20:30:14 -0600

    meeting endpoint specs... can't figure out how to throw 500 server errors

---
+++ data/db.js
@@ -0,0 +1,12 @@
+[
+    {
+        "name": "Tarzan",
+        "bio": "Me swing",
+        "id": "vJf4Mawwl"
+    },
+    {
+        "name": "Jane",
+        "bio": "Me too, but better",
+        "id": "5OkD2R3pw"
+    }
+]
--- index.js
+++ index.js
@@ -1,46 +1,79 @@
-const express = require('express');
-const shortid = require('shortid');
+const express = require("express");
+const shortid = require("shortid");
 const PORT = 5000;
 const server = express();
 
 server.use(express.json());
 
-let users = [];
+const users = [];
 
-server.post('/api/users', (req, res) => {
-    const userInfo = req.body;
-    userInfo.id = shortid.generate();
-    users.push(userInfo)
 
-    res.status(201).json(userInfo);
-})
+server.post("/api/users", (req, res) => {
+  const { name, bio } = req.body;
+  if (!name || !bio) {
+    res
+      .status(400)
+      .json({ errorMessage: "Please provide name and bio for the user." });
+    } else {
+        const userInfo = req.body;
+        userInfo.id = shortid.generate();
+    
+        users.push(userInfo)
+        res.status(201).json(userInfo);
+      }
+    });
 
 server.get('/api/users', (req, res) => {
-    res.status(200).json(users)
-})
+    res.status(200).json(users);
+  });
 
-server.get('/api/users/:id', (req, res) =>{
-    const { id } = req.params;
-    const found = users.find(user => user.id === id);
-    if (found) {
-        res.status(200).json(found);
-    }else {
-        res.status(404).json({message: "user not found"})
+server.get("/api/users/:id", (req, res) => {
+  const { id } = req.params;
+  const found = users.find((user) => user.id === id);
+  if (found) {
+    res.status(200).json(found);
+  } else {
+    res
+      .status(404)
+      .json({ message: "The user with the specified ID does not exist." });
+  }
+});
+
+server.delete("/api/users/:id", (req, res) => {
+  const { id } = req.params;
+  const found = users.find((user) => user.id === id);
+  if (found) {
+    users = users.filter((user) => user.id !== id);
+    res.status(200).json(found);
+  } else {
+    res
+      .status(404)
+      .json({ message: "The user with the specified ID does not exist." });
+  }
+});
+
+server.put("/api/users/:id", (req, res) => {
+  const { id } = req.params;
+  const { name, bio } = req.body
+  const changes = req.body;
+  let index = users.findIndex((user) => user.id === id);
+
+  if (!name || !bio) {
+    res
+      .status(400)
+      .json({ errorMessage: "Please provide name and bio for the user." });
     }
-
-})
-
-server.delete('/api/users/:id', (req, res) => {
-    const { id } = req.params;
-    const found = users.find(user => user.id === id);
-    if (found) {
-        users = users.filter(user => user.id !== id);
-        res.status(200).json(found);
-    }else {
-        res.status(404).json({message: "user not found"})
-    }
-})
+  else if (index !== -1) {
+    changes.id = id;
+    users[index] = changes;
+    res.status(200).json(users[index]);
+  } else {
+    res
+      .status(404)
+      .json({ message: "The user with the specified ID does not exist." });
+  }
+});
 
 server.listen(PORT, () => {
-    console.log(`listening on http://localhost:${PORT}`);
+  console.log(`listening on http://localhost:${PORT}`);
 });
