34b1ed017fd9e68da5f9bcb19f33fb44accdbff8
Elijah Atkins <elijahorama@gmail.com>
2020-07-08 19:30:11 -0600

    get single user by id working with error

--- index.js
+++ index.js
@@ -19,6 +19,17 @@ server.get('/api/users', (req, res) => {
     res.status(200).json(users)
 })
 
+server.get('/api/users/:id', (req, res) =>{
+    const { id } = req.params;
+    const found = users.find(user => user.id === id);
+    if (found) {
+        res.status(200).json(found);
+    }else {
+        res.status(404).json({message: "user not found"})
+    }
+
+})
+
 server.listen(PORT, () => {
     console.log(`listening on http://localhost:${PORT}`);
 });
